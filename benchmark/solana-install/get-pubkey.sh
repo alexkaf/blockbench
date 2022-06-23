4d874dc72ef37199e52f4f337f54a87f13adbae4
alexkaf <alexkafiris@gmail.com>
2022-06-24 00:46:06 +0300

    README.md: How to run deploy.js

--- benchmark/solana-install/get-pubkey.sh
+++ benchmark/solana-install/get-pubkey.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"
-BOOTSTRAP_NODE=$(head -n 1 hosts)
+here=$(dirname "$0")
+source $here/env.sh
+
+export PATH="$PATH:$SOLANA/target/release"
+
+BOOTSTRAP_NODE=$(head -n 1 $here/hosts)
 
 solana config set --url http://$BOOTSTRAP_NODE:8899/ > /dev/null
 
--- benchmark/solana-install/start-cluster.sh
+++ benchmark/solana-install/start-cluster.sh
@@ -4,9 +4,7 @@ here=$(dirname "$0")
 source $here/env.sh
 
 function start_client() {
-    sleep 15
-    echo $1 WAITING FOR LEADER TO GENERATE FIRST SNAPSHOT
-    sleep 45
+    sleep 60
     ssh -oStrictHostKeyChecking=no root@$host $BLOCKBENCH/validator.sh $host $idx &
 }
 
@@ -15,7 +13,8 @@ for host in $(cat hosts)
 do
   if [ $idx -eq 0 ]
   then
-    ssh -oStrictHostKeyChecking=no root@$host $BLOCKBENCH/validator.sh $host $idx &
+    ssh -oStrictHostKeyChecking=no root@$host $BLOCKBENCH/validator.sh $host $idx
+    echo WAITING FOR LEADER TO GENERATE FIRST SNAPSHOT
   else
     start_client $host $idx &
   fi
