6cf7a6aa31074482aa6d2f2ceb6e39a53e6fe7d5
alexkaf <alexkafiris@gmail.com>
2022-06-24 15:45:54 +0300

    README.md: How to run deploy.js

--- benchmark/solana-install/install-dependencies.sh
+++ benchmark/solana-install/install-dependencies.sh
@@ -5,18 +5,20 @@ source $here/env.sh
 
 # Install dependencies
 
-sudo apt-get update sudo && apt-get upgrade -y && sudo apt install curl dirmngr apt-transport-https lsb-release ca-certificates vim
+sudo apt-get update && apt-get upgrade -y && sudo apt install curl dirmngr apt-transport-https lsb-release ca-certificates vim
 sudo apt-get install -y libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make libprotobuf-dev protobuf-compiler jq psmisc git vim
 
 # Install node
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt install -y nodejs gcc g++ make
+#curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+#sudo apt install -y nodejs gcc g++ make
 
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-#source ~/.bashrc
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc
 
 nvm install v16.15.1
 nvm use v16.15.1
+
+npm install npm@latest -g
 
 # Installs rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
