ed3712e8a81063af781381ea12cc61331a5ad0ab
alexkaf <alexkafiris@gmail.com>
2022-06-25 13:48:38 +0300

    README.md: How to run deploy.js

--- benchmark/solana-install/environment/install-to-all.sh
+++ benchmark/solana-install/environment/install-to-all.sh
@@ -8,6 +8,7 @@ function install() {
   ssh -oStrictHostKeyChecking=no ubuntu@"$1" "cd $INSTALLS && ./install.sh"
 }
 
+# shellcheck disable=SC2013
 for host in $(cat "$HERE"/../hosts)
 do
   install "$host" &
