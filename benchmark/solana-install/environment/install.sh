be1b564f65510e6ea875d58989d000aa4d9ad3a7
alexkaf <alexkafiris@gmail.com>
2022-06-25 13:38:40 +0300

    README.md: How to run deploy.js

--- benchmark/solana-install/environment/install.sh
+++ benchmark/solana-install/environment/install.sh
@@ -4,10 +4,4 @@ HERE=$(dirname "$0")
 source "$HERE/../env.sh"
 #PACKAGES=$HERE/packages
 
-function install() {
-  echo $BLOCKBENCH
-#  "$PACKAGES"/system.sh
-#  "$PACKAGES"/node.sh
-#  "$PACKAGES"/cargo.sh
-#"$PACKAGES"/solana/install.sh
-}
+echo "$BLOCKBENCH"
