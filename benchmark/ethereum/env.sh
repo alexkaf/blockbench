7dda9056493e06e88f970d2e0d8a215e8179a288
alexkaf <alexkafiris@gmail.com>
2022-06-27 15:28:10 +0300

    README.md: How to run deploy.js

--- benchmark/ethereum/env.sh
+++ benchmark/ethereum/env.sh
@@ -1,9 +1,9 @@
 ETH_HOME=/home/ubuntu/assesments-new/blockbench/benchmark/ethereum
 HOSTS=$ETH_HOME/hosts
 CLIENTS=$ETH_HOME/clients
-ETH_DATA=/root/ethereum/data
+ETH_DATA=/home/ubuntu/ethereum/data
 LOG_DIR=$ETH_HOME/logs
-EXE_HOME=/root/assesments/blockbench/benchmark/src/donothing
+EXE_HOME=/home/ubuntu/assesments/blockbench/benchmark/src/donothing
 BENCHMARK=ycsb
 
 ##comment these out for smallbank
