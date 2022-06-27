e442109611b0ac7d9c99159af16c0baabd92a198
alexkaf <alexkafiris@gmail.com>
2022-06-27 15:58:40 +0300

    README.md: How to run deploy.js

--- benchmark/ethereum/start-mining.sh
+++ benchmark/ethereum/start-mining.sh
@@ -2,7 +2,19 @@
 cd `dirname ${BASH_SOURCE-$0}`
 . env.sh
 
-geth --datadir=$ETH_DATA --http.api="db,eth,net,web3,personal,web3" --http --http.addr 0.0.0.0 --allow-insecure-unlock --ws --ws.addr 0.0.0.0 --networkid 123454321 --unlock 0 --password <(echo -n "") --mine --miner.threads 8 > $ETH_DATA/log 2>&1 &
+geth --datadir=$ETH_DATA \
+     --http.api="db,eth,net,web3,personal,web3" \
+     --http \
+     --http.addr 0.0.0.0 \
+     --allow-insecure-unlock \
+     --ws \
+     --ws.addr 0.0.0.0 \
+     --networkid 123454321 \
+     --unlock 0 \
+     --password <(echo -n "") \
+     --mine \
+     --nat extip:10.201.252.8 \
+     --miner.threads 8 > $ETH_DATA/log 2>&1 &
 
 sleep 1
 
