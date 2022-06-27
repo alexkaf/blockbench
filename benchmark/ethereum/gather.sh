5050cb077795d699d3ce095ec30a94e132c787a0
alexkaf <alexkafiris@gmail.com>
2022-06-27 16:46:55 +0300

    README.md: How to run deploy.js

--- benchmark/ethereum/gather.sh
+++ benchmark/ethereum/gather.sh
@@ -4,9 +4,9 @@ cd `dirname ${BASH_SOURCE-$0}`
 . env.sh 
 
 i=0
-for host in `cat $HOSTS`; do
+for host in `cat hosts`; do
   if [[ $i -lt $1 ]]; then
-    echo "admin.addPeer("`ssh root@$host $ETH_HOME/enode.sh $host 2>/dev/null | grep enode`")" >> addPeer.txt
+    echo "admin.addPeer("ssh "$CURRENT_USER@$host" "$ETH_HOME"/enode.sh")" >> addPeer.txt
   fi
   let i=$i+1
   echo $i
