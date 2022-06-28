d7b45d56062d2490c72256e56d94b106dfd1b81e
alexkaf <alexkafiris@gmail.com>
2022-06-28 21:15:13 +0300

    README.md: How to run deploy.js

---
+++ benchmark/solana-install/environment/solana/compile_programs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+HERE=$(dirname "$0")
+source "$HERE/../env.sh"
+
+CONTRACTS="$ASSESMENTS"/blockbench/benchmark/contracts/solana
+
+PROGRAMS=( "kvstore" "donothing" "smallbank" "ioheavy" "cpuheavy" )
+
+for program in $PROGRAMS; do
+  cd "$CONTRACTS"/"$program"
+  cargo build-bpf
+done
