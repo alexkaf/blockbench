88dc1333450a3517463d44cd9bd46f31a3d33cd5
alexkaf <alexkafiris@gmail.com>
2022-06-28 20:53:54 +0300

    README.md: How to run deploy.js

--- benchmark/solana-install/environment/solana/install.sh
+++ benchmark/solana-install/environment/solana/install.sh
@@ -20,6 +20,6 @@ cargo build --release
 sh -c "$(curl -sSfL https://release.solana.com/v1.10.24/install)"
 mv "$HOME"/.local/share/solana/install/active_release/bin/sdk "$HOME"/assesments/blockbench/benchmark/solana-install/solana/target/release
 
-export PATH="$PATH":"$HOME"/assesments/blockbench/benchmark/solana-install/solana/target/release
+echo export PATH="$PATH":"$HOME"/assesments/blockbench/benchmark/solana-install/solana/target/release >> ~/.bashrc
 
 head -n -1 ~/.profile | sponge ~/.profile
