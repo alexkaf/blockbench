9ca2095de3f6bc4c677dd0796abf78b6f21cd405
alexkaf <alexkafiris@gmail.com>
2022-06-28 22:19:35 +0300

    README.md: How to run deploy.js

--- benchmark/ethereum/start-all.sh
+++ benchmark/ethereum/start-all.sh
@@ -6,20 +6,10 @@ cd `dirname ${BASH_SOURCE-$0}`
 i=0
 for host in `cat hosts`; do
   if [[ $i -lt $1 ]]; then
-    ssh -oStrictHostKeyChecking=no $CURRENT_USER@$host $ETH_HOME/start-mining.sh $host
+    ssh -oStrictHostKeyChecking=no $CURRENT_USER@$host $ETH_HOME/start-mining.sh $host $1
     echo done node $host
   fi
   let i=$i+1
-done
-
-rm -rf addPeer.txt
-
-./gather.sh $1
-
-for host in $(cat hosts); do
-  for com in $(cat addPeer.txt); do
-    ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$host" "geth attach ~/ethereum/data/geth.ipc --exec $com"
-  done
 done
 
 #sleep 3
--- benchmark/ethereum/start-mining.sh
+++ benchmark/ethereum/start-mining.sh
@@ -16,4 +16,11 @@ geth --datadir=$ETH_DATA \
      --nat extip:$1 \
      --miner.threads 8 > $ETH_DATA/log 2>&1 &
 
-sleep 10
+sleep 20
+
+rm -rf addPeer.txt
+./gather.sh $2
+
+for com in $(cat addPeer.txt); do
+  ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$host" "geth attach ~/ethereum/data/geth.ipc --exec $com"
+done
