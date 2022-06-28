6828721b449a9a5b4fdb196d1a37056039900ca8
alexkaf <alexkafiris@gmail.com>
2022-06-28 19:36:01 +0300

    README.md: How to run deploy.js

--- benchmark/ethereum/install-dependencies.sh
+++ benchmark/ethereum/install-dependencies.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+HERE=$(dirname "$0")
+
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:ethereum/ethereum
 
 sudo apt-get update
 sudo apt-get install -y ethereum
+
+
+git clone https://github.com/mrtazz/restclient-cpp.git
+
+sudo apt-get install -y autotools-dev automake libtool libcurl4-nss-dev
+git apply "$HERE"/lcurl.patch
+
+cd restclient-cpp
+
+./autogen.sh
+./configure
+sudo make install
