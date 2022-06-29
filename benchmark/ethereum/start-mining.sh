83c5ab46cd68f5873e0f7096f14a57501e2065e0
alexkaf <alexkafiris@gmail.com>
2022-06-29 09:31:41 +0300

    README.md: How to run deploy.js

--- benchmark/ethereum/start-mining.sh
+++ benchmark/ethereum/start-mining.sh
@@ -16,9 +16,11 @@ geth --datadir=$ETH_DATA \
      --nat extip:$1 \
      --miner.threads 8 > $ETH_DATA/log 2>&1 &
 
-exit 0
+wait
 echo Waiting 60 seconds...
 sleep 60
+
+exit 1
 
 ./gather.sh $2
 
