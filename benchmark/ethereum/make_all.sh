fb90c1cb659c313b1f2c52f5e2c3109001715626
alexkaf <alexkafiris@gmail.com>
2022-07-01 12:22:13 +0300

    README.md: How to run deploy.js

--- benchmark/ethereum/make_all.sh
+++ benchmark/ethereum/make_all.sh
@@ -4,8 +4,8 @@ cd `dirname ${BASH_SOURCE-$0}`
 . env.sh
 
 function make() {
-    ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$1" "cd /home/ubuntu/assesments/blockbench/src/macro/kvstore && make"
-    ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$1" "cd /home/ubuntu/assesments/blockbench/src/macro/smallbank && make"
+    ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$1" "cd /home/ubuntu/assesments/blockbench/src/macro/kvstore && make clean && make"
+    ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$1" "cd /home/ubuntu/assesments/blockbench/src/macro/smallbank && make clean && make"
 }
 
 for host in $(cat hosts); do
