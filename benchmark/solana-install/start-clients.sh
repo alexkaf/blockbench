6d447eda742b230abd889f14ecbe62c1d624f635
alexkaf <alexkafiris@gmail.com>
2022-07-29 01:49:37 +0300

    README.md: How to run deploy.js

--- benchmark/solana-install/start-clients.sh
+++ benchmark/solana-install/start-clients.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# Threads Txrate Workload
 here=$(dirname "$0")
 source $here/env.sh
 
 for client in $(cat clients); do
-  ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$client" "cd /home/ubuntu/assesments/blockbench/src/macro/rust-driver && source ~/.cargo/env && cargo run -- -db solana -endpoint http://localhost:8899 -txrate 8 -threads 4 -P /home/ubuntu/assesments/blockbench/src/macro/kvstore/workloads/workload$1.spec " &
+  ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$client" "cd /root/assesments/blockbench/src/macro/rust-driver && source ~/.cargo/env && cargo run -- -db solana -endpoint http://localhost:8899 -txrate $1 -threads $2 -P /root/assesments/blockbench/src/macro/kvstore/workloads/workload$3.spec " &
 done
