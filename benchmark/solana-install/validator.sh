25d52a58951497406e9cd23cc8d2ae0b3de78e33
alexkaf <alexkafiris@gmail.com>
2022-07-29 18:14:16 +0300

    README.md: How to run deploy.js

--- benchmark/solana-install/validator.sh
+++ benchmark/solana-install/validator.sh
@@ -23,7 +23,7 @@ then
 
   # Create genesis
   echo CREATING BOOTSTRAP NODE GENESIS
-  ./multinode-demo/setup.sh  --bootstrap-validator-stake-lamports 5000000000 --cluster-type development
+  ./multinode-demo/setup-from-mainnet-beta.sh  --bootstrap-validator-stake-lamports 5000000000 --cluster-type development
 
   # Start faucet
   echo STARTING FAUCET
