6340b3219a3a515ad4eb322bf244c0e779fcf44c
akafiris <alexkafiris@gmail.com>
2022-07-31 18:03:12 +0200

    fix typo

--- benchmark/solana-install/compile_all.sh
+++ benchmark/solana-install/compile_all.sh
@@ -5,7 +5,7 @@ source $here/env.sh
 
 COMMAND=". ~/.cargo/env"
 COMMAND="$COMMAND && export PATH=$PATH:/home/ubuntu/assesments/blockbench/benchmark/solana-install/solana/target/release"
-COMMAND="$COMMAND && cd $ASSESMENTS/blockbench/src/macro/rust_driver && cargo build"
+COMMAND="$COMMAND && cd $ASSESMENTS/blockbench/src/macro/rust-driver && cargo build"
 
 for host in $(cat hosts)
 do
