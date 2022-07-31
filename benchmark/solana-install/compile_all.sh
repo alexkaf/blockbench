7827e317fe6ef0a39f24bfa30171b9dcb6e925eb
alexkaf <alexkafiris@gmail.com>
2022-07-31 19:08:18 +0300

    fix evaluations

--- benchmark/solana-install/compile_all.sh
+++ benchmark/solana-install/compile_all.sh
@@ -3,9 +3,7 @@
 here=$(dirname "$0")
 source $here/env.sh
 
-COMMAND=". ~/.cargo/env"
-COMMAND="$COMMAND && export PATH=\$PATH:/home/ubuntu/assesments/blockbench/benchmark/solana-install/solana/target/release"
-COMMAND="$COMMAND && cd \$ASSESMENTS/blockbench/src/macro/rust-driver && cargo build"
+COMMAND=". ~/.cargo/env && && export PATH=$PATH:/home/ubuntu/assesments/blockbench/benchmark/solana-install/solana/target/release && cd $ASSESMENTS/blockbench/src/macro/rust-driver && cargo build"
 
 for host in $(cat hosts)
 do
