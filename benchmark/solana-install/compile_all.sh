16424289cef6b9b482b6e7024402ae0685915695
akafiris <alexkafiris@gmail.com>
2022-07-31 17:54:16 +0200

    Compile bash script

---
+++ benchmark/solana-install/compile_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+here=$(dirname "$0")
+source $here/env.sh
+
+for host in $(cat hosts)
+do
+    ssh -oStrictHostKeyChecking=no $CURRENT_USER@$host ". ~/.cargo/env \
+    && export PATH=$PATH:/home/ubuntu/assesments/blockbench/benchmark/solana-install/solana/target/release \
+    && cd $ASSESMENTS/blockbench/src/macro/rust_driver && cargo build" &
+done
