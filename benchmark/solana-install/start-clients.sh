a69c0499b364e55451a9a0b2bcd486c209536548
akafiris <alexkafiris@gmail.com>
2022-07-31 13:47:36 +0200

    Fix done to fi

--- benchmark/solana-install/start-clients.sh
+++ benchmark/solana-install/start-clients.sh
@@ -8,7 +8,7 @@ idx=0
 for client in $(cat clients); do
   if [ $idx -eq "$1" ] then 
     exit 0
-  done
+  fi
 
   ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$client" "cd /$SERVER_HOME/assesments/blockbench/src/macro/rust-driver && source ~/.cargo/env && cargo run -- -db solana -endpoint http://localhost:8899 -txrate $2 -threads $3 -P /root/assesments/blockbench/src/macro/kvstore/workloads/workload$4.spec " &
 
