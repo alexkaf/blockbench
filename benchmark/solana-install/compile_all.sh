32b4914a51813920a012cbf4cfb9978669a562c5
alexkaf <alexkafiris@gmail.com>
2022-07-31 19:08:48 +0300

    fix evaluations

--- benchmark/solana-install/compile_all.sh
+++ benchmark/solana-install/compile_all.sh
@@ -3,9 +3,7 @@
 here=$(dirname "$0")
 source $here/env.sh
 
-COMMAND=". ~/.cargo/env && && export PATH=$PATH:/home/ubuntu/assesments/blockbench/benchmark/solana-install/solana/target/release && cd $ASSESMENTS/blockbench/src/macro/rust-driver && cargo build"
-
 for host in $(cat hosts)
 do
-    ssh -oStrictHostKeyChecking=no $CURRENT_USER@$host $COMMAND &
+    ssh -oStrictHostKeyChecking=no $CURRENT_USER@$host ". ~/.cargo/env && && export PATH=$PATH:/home/ubuntu/assesments/blockbench/benchmark/solana-install/solana/target/release && cd $ASSESMENTS/blockbench/src/macro/rust-driver && cargo build" &
 done
