a24f4c5108cde91c36e803ff9946830afb213f4e
akafiris <alexkafiris@gmail.com>
2022-08-02 19:19:13 +0200

    have smallbank in client

--- benchmark/solana-install/start-clients.sh
+++ benchmark/solana-install/start-clients.sh
@@ -6,12 +6,16 @@ source $here/env.sh
 
 idx=0
 for client in $(cat clients); do
-  if [ $idx -eq "$1" ] 
+  if [ $idx -eq "$2" ] 
   then 
     exit 0
   fi
 
-  ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$client" "cd $SERVER_HOME/assesments/blockbench/src/macro/rust-driver && source ~/.cargo/env && cargo run -- -db solana -endpoint http://localhost:8899 -txrate $3 -threads $2 -P $SERVER_HOME/assesments/blockbench/src/macro/kvstore/workloads/workload$4.spec " &
+  if [ $1 -eq "smallbank" ]
+  then 
+    ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$client" "cd $SERVER_HOME/assesments/blockbench/src/macro/rust-driver && source ~/.cargo/env && cargo run -- -db solana -endpoint http://localhost:8899 -txrate $4 -threads $3 -wl smallbank -ops $5" &
+  fi
+  ssh -oStrictHostKeyChecking=no "$CURRENT_USER@$client" "cd $SERVER_HOME/assesments/blockbench/src/macro/rust-driver && source ~/.cargo/env && cargo run -- -db solana -endpoint http://localhost:8899 -txrate $4 -threads $3 -P $SERVER_HOME/assesments/blockbench/src/macro/kvstore/workloads/workload$5.spec " &
 
   let idx=$idx+1
 done
