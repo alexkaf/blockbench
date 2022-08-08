e26e25f058de923c5a172c60c83da6549a880cb2
akafiris <alexkafiris@gmail.com>
2022-08-08 20:18:04 +0200

    minor fix

--- src/micro/transactions/ethereum/transactions/driver.js
+++ src/micro/transactions/ethereum/transactions/driver.js
@@ -3,5 +3,5 @@ const eth = require('./eth.js');
 
 const argsObject = parser();
 
-const p = eth.getProvider(argsObject["endpoint"]);
+const p = eth.getProvider(argsObject["endpoint"][0]);
 console.log(p.eth.getBlockNumber());
