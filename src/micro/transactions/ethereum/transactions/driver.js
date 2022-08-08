4595b28561eb041cf95671f0405ee5af6cb40235
akafiris <alexkafiris@gmail.com>
2022-08-08 20:17:30 +0200

    fix driver

--- src/micro/transactions/ethereum/transactions/driver.js
+++ src/micro/transactions/ethereum/transactions/driver.js
@@ -1,5 +1,7 @@
 const { parser } = require('./parser.js');
 const eth = require('./eth.js');
 
-const p = eth.getProvider('localhost');
+const argsObject = parser();
+
+const p = eth.getProvider(argsObject["endpoint"]);
 console.log(p.eth.getBlockNumber());
