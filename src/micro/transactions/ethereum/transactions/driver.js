56a93dc7cb0e4b07244dbe30b8982b58a55b79ad
akafiris <alexkafiris@gmail.com>
2022-08-08 20:22:41 +0200

    fix

--- src/micro/transactions/ethereum/transactions/driver.js
+++ src/micro/transactions/ethereum/transactions/driver.js
@@ -7,5 +7,5 @@ const p = eth.getProvider(argsObject["endpoint"][0]);
 
 (async() => {
     console.log(await p.eth.getBlockNumber());
-    console.log(await p.collectAccounts(p));
+    console.log(await eth.collectAccounts(p));
 })();
