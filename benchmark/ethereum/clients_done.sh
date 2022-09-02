9cc82c02e1bf7e82e7a3e03cba0fcc984b26b04e
alexkaf <alexkafiris@gmail.com>
2022-09-02 17:39:49 +0300

    all good :)

---
+++ benchmark/ethereum/clients_done.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+idx=0
+for host in `cat hosts`; do
+    if [[ $idx -lt $1 ]]; then
+        scp ubuntu@$host:~/test.txt .test_$idx &
+    fi
+    let idx=$idx+1
+done
+
+wait
+cat .test_* > .temp
+rm .test_*
+
+TESTS_DONE=$(cat .temp | grep End | wc -l)
+
+if [ "$TESTS_DONE" = "$1" ]; then
+    echo true
+fi
