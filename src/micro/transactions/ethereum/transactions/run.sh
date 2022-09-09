9d3e3ffe76cc34790eb9d1906a0c59f65f7969e2
alexkaf <alexkafiris@gmail.com>
2022-09-09 16:38:04 +0000

    cadsfcd

--- src/micro/transactions/ethereum/transactions/run.sh
+++ src/micro/transactions/ethereum/transactions/run.sh
@@ -2,4 +2,5 @@
 
 cd /home/ubuntu/assesments/blockbench/src/micro/transactions/ethereum/transactions
 
-node driver.js -k $1 -total $2 $3 -txs $4 -r $5 -name $6
+# node driver.js -k $1 -total $2 $3 -txs $4 -r $5 -name $6
+node driver.js $@
